bc954bb34ccecb76a43b0c9a8dc8a0fdbcfbd5d1
Daniyar111 <dani.changylov@gmail.com>
2019-07-04 04:13:17 -0700

    array_rotations

---
+++ bin/main_array.dart
@@ -0,0 +1,6 @@
+import 'package:DartProblems/array/array_rotations.dart';
+
+main(List<String> arguments) async {
+
+  print(arrayLeftRotateUsingTemp([1, 2, 3, 4, 5, 6, 7], 2));
+}
---
+++ lib/array/array_rotations.dart
@@ -0,0 +1,13 @@
+
+List<int> arrayLeftRotateUsingTemp(List<int> array, int rotateSize){
+
+  List<int> temp = array.sublist(0, rotateSize);
+  array.removeRange(0, rotateSize);
+  array.insertAll(array.length, temp);
+  return array;
+}
+
+List<int> arrayLeftRotateOneByOne(List<int> array, int rotateSize){
+
+
+}
