4fab347d389008c4439831c933c62137235e9599
Daniyar111 <dani.changylov@gmail.com>
2019-07-10 06:10:22 -0700

    double added

--- lib/dart_tour/core/core_double.dart
+++ lib/dart_tour/core/core_double.dart
@@ -1,8 +1,9 @@
+
 void coreDouble(){
 
   double a = 0.0;
-  double b = 2.33;
-  double c = -2.33;
+  double b = 2.334;
+  double c = -2.334;
   double d = -0.0;
 
   /// ----------- Properties ------------
@@ -30,8 +31,8 @@ void coreDouble(){
   /// ------------ Methods -------------
 
   print(a.abs()); // 0.0
-  print(b.abs()); // 2.33
-  print(c.abs()); // 2.33
+  print(b.abs()); // 2.334
+  print(c.abs()); // 2.334
   print(d.abs()); // 0.0
 
   print(a.ceil()); // 0
@@ -42,15 +43,56 @@ void coreDouble(){
   print(a.ceilToDouble()); // 0.0
   print(b.ceilToDouble()); // 3.0
   print(c.ceilToDouble()); // -2.0
-  print(d.ceilToDouble()); // 0.0
+  print(d.ceilToDouble()); // -0.0
 
   print(a.floor()); // 0
   print(b.floor()); // 2
   print(c.floor()); // -3
   print(d.floor()); // 0
 
-  print(a.floorToDouble()); // 0
-  print(b.floorToDouble()); // 2
-  print(c.floorToDouble()); // -3
-  print(d.floorToDouble()); // 0
+  print(a.floorToDouble()); // 0.0
+  print(b.floorToDouble()); // 2.0
+  print(c.floorToDouble()); // -3.0
+  print(d.floorToDouble()); // -0.0
+
+  // same mod
+  print(b.remainder(1.2)); // 1.1340000000000001
+
+  print(a.round()); // 0
+  print(b.round()); // 2
+  print(c.round()); // -2
+  print(d.round()); // 0
+
+  print(a.roundToDouble()); // 0.0
+  print(b.roundToDouble()); // 2.0
+  print(c.roundToDouble()); // -2.0
+  print(d.roundToDouble()); // -0.0
+
+  print(a.truncate()); // 0
+  print(b.truncate()); // 2
+  print(c.truncate()); // -2
+  print(d.truncate()); // 0
+
+  print(a.truncateToDouble()); // 0.0
+  print(b.truncateToDouble()); // 2.0
+  print(c.truncateToDouble()); // -2.0
+  print(d.truncateToDouble()); // -0.0
+
+
+//  a = math.max(-1, math.min(1, a));
+//  print(a);
+
+  /// ------------ Static Methods -------------
+
+  print(double.parse('1.2'));  // 1.2
+  print(double.tryParse('1,2'));  // null
+
+
+  /// ------------ Constants -------------
+
+  print(double.infinity);  // Infinity -> 1.0 / 0.0
+  print(double.maxFinite); // 1.7976931348623157e+308
+  print(double.minPositive); // 5e-324
+  print(double.nan); // NaN -> 0.0 / 0.0
+  print(double.negativeInfinity);  // -Infinity -> -(1.0/0.0)
 }
