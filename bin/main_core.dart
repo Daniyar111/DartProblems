451d338555ec9f1105a088227e808d5983071b47
Daniyar111 <dani.changylov@gmail.com>
2019-07-11 02:40:23 -0700

    object added

--- bin/main_core.dart
+++ bin/main_core.dart
@@ -3,8 +3,9 @@ import 'package:DartProblems/dart_tour/core/core_bool.dart';
 import 'package:DartProblems/dart_tour/core/core_double.dart';
 import 'package:DartProblems/dart_tour/core/core_int.dart';
 import 'package:DartProblems/dart_tour/core/core_num.dart';
+import 'package:DartProblems/dart_tour/core/core_object.dart';
 
 main(List<String> arguments) async {
 
-  coreBigInt();
+  coreObject();
 }
---
+++ lib/dart_tour/core/core_object.dart
@@ -0,0 +1,40 @@
+class Person{
+
+  final int id;
+
+  Person(this.id);
+
+  @override
+  noSuchMethod(Invocation invocation) {
+    return 'Got the ${invocation.memberName} with arguments ${invocation.positionalArguments}';
+  }
+
+  @override
+  bool operator ==(other) {
+    return other is Person && other.id == id;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode;
+  }
+
+  @override
+  String toString() {
+    return 'Person has $id id';
+  }
+
+}
+
+void coreObject(){
+
+  dynamic person = Person(356);
+  dynamic person1 = Person(356);
+
+  print(person.missing('Daniyar', 24));
+  print(person.runtimeType);
+  print(person.toString());
+
+  print(person == person1); // true
+  print(identical(person, person1)); // false, cause they are not references to the same object
+}
